e3058a134479e347d31b6def483cc1eae790920f
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-09-25 20:39:39 -0300

    implementation of kafka logic, with producer and consumer. Containers in docker for projects and docker-compose to orchestrate the containers.

---
+++ Consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Consumer
+{
+    using Confluent.Kafka;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "kafka:29092",
+                GroupId = "qualquer coisa",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("cat");
+
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    cancellationTokenSource.Cancel();
+                };
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumerResult = consumer.Consume(cancellationTokenSource.Token);
+                            var message = consumerResult.Message.Value;
+                            Console.WriteLine(message);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+
+                consumer.Close();
+            }
+
+        }
+    }
+}
---
+++ WebApi/Controllers/ProducerController.cs
@@ -0,0 +1,58 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private readonly string directory;
+        private readonly IProducer<Null, string> kafkaProducer;
+
+        public ProducerController()
+        {
+            // Configurar a pasta de destino
+            directory = Path.Combine(Directory.GetCurrentDirectory(), "files");
+
+            // Configurar o produtor Kafka (substitua pelos detalhes do seu ambiente)
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "kafka:29092"
+            };
+            kafkaProducer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        [HttpPost]
+        [Route("cat")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                string randomName = SetRandomName(file.FileName);
+                string fullPath = Path.Combine(directory, randomName);
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Enviar o nome da imagem para o Kafka (ou o conteúdo da imagem, se preferir)
+                kafkaProducer.Produce("cat", new Message<Null, string>
+                {
+                    Value = randomName // Pode ser o nome ou o conteúdo da imagem, dependendo das suas necessidades
+                });
+
+                return Ok(new { mensagem = "Imagem salva com sucesso!" });
+            }
+            return BadRequest("Nenhuma imagem enviada.");
+        }
+
+        private string SetRandomName(string originalName)
+        {
+            string baseName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+            string extension = Path.GetExtension(originalName);
+            return $"{baseName}{extension}";
+        }
+    }
+}
