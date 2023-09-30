eec0ba856d8969cf1da55902a5b072700a3493be
Lucas Leite <lcslrnjr@gmail.com>
2023-09-30 13:59:52 -0300

    Atualizacao no recebimento de extensao de foto.
    Alteracao no BadResquest para Unsupported Media Type (415).

--- src/WebApi/Controllers/ProducerController.cs
+++ src/WebApi/Controllers/ProducerController.cs
@@ -12,6 +12,7 @@ namespace WebApi.Controllers
     {
         private readonly string directory;
         private readonly IKafkaProducerService _kafkaProducer;
+        private readonly string[] allowedExtensions = { ".jpg", ".jpeg", ".png", ".gif" };
 
         public ProducerController(IKafkaProducerService kafkaProducer)
         {
@@ -23,38 +24,47 @@ namespace WebApi.Controllers
         [Route("cat")]
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Nenhuma imagem enviada.");
+            }
+
             if (file.Length > 5000000)
             {
-                throw new BadHttpRequestException("Tamanho do arquivo maior que 5MB", 413);
+                throw new BadHttpRequestException("A imagem não pode ser maior que 5MB.", 413);
             }
-            if (file != null && file.Length > 0)
+
+            string fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+            if (!allowedExtensions.Contains(fileExtension))
             {
-                string randomName = SetRandomName(file.FileName);
-                string fullPath = Path.Combine(directory, randomName);
-
-                using (var stream = new FileStream(fullPath, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
-
-                byte[] fileData;
-                using (var memoryStream = new MemoryStream())
-                {
-                    await file.CopyToAsync(memoryStream);
-                    fileData = memoryStream.ToArray();
-                }
-                bool success = await _kafkaProducer.ProduceMessageAsync("cat", randomName, fileData);
-                if (success)
-                {
-                    return Ok(new { mensagem = "Imagem salva com sucesso!" });
-                }
-                else
-                {
-                    throw new Exception("Ocorreu algum erro no envio da mensagem");
-                }
-
+                return StatusCode(415, "Tipo de mídia não suportado. Apenas imagens com as seguintes extensões são permitidas: " + string.Join(", ", allowedExtensions));
             }
-            return BadRequest("Nenhuma imagem enviada.");
+
+            string randomName = SetRandomName(file.FileName);
+            string fullPath = Path.Combine(directory, randomName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            byte[] fileData;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                fileData = memoryStream.ToArray();
+            }
+
+            bool success = await _kafkaProducer.ProduceMessageAsync("cat", randomName, fileData);
+            if (success)
+            {
+                return Ok(new { mensagem = "Imagem salva com sucesso!" });
+            }
+            else
+            {
+                return StatusCode(500, "Ocorreu algum erro no envio da mensagem");
+            }
         }
 
         private static string SetRandomName(string originalName)
