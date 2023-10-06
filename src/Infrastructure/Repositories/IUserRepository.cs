33697099b269780671696f509b157ed6d7b58ff4
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2023-10-06 18:13:29 -0300

    preparing for jwt tokens \,,/_

--- src/Infrastructure/Repositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infrastructure.Repositories
-{
-	using Domain.DTO;
-
-	public interface IUserRepository
-	{
-		Task<bool> Register(RegisterDTO registerDTO);
-	}
-}
--- src/Infrastructure/Security/SecurityService.cs
+++ src/Infrastructure/Security/SecurityService.cs
@@ -4,6 +4,10 @@
 	using Domain.Entities;
 	using Infrastructure.ApplicationContext;
 	using Microsoft.AspNetCore.Identity;
+	using DotNetEnv;
+	using Microsoft.AspNetCore.Authentication.JwtBearer;
+	using Microsoft.IdentityModel.Tokens;
+	using System.Text;
 
 	public static class SecurityService
 	{
@@ -14,29 +18,29 @@
 				.AddDefaultTokenProviders();
 		}
 
-		//public static void AddAppAuthentication(this IServiceCollection services)
-		//{
-		//	Env.Load();
+		public static void AddAppAuthentication(this IServiceCollection services)
+		{
+			Env.Load();
 
-		//	services.AddAuthentication(options =>
-		//	{
-		//		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-		//		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-		//		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-		//	})
-		//		.AddJwtBearer(options =>
-		//		{
-		//			options.SaveToken = true;
-		//			options.RequireHttpsMetadata = false;
-		//			options.TokenValidationParameters = new TokenValidationParameters()
-		//			{
-		//				ValidateIssuer = true,
-		//				ValidateAudience = true,
-		//				ValidAudience = Env.GetString("JWT_VALID_AUDIENCE"),
-		//				ValidIssuer = Env.GetString("JWT_VALID_ISSUER"),
-		//				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Env.GetString("JWT_SECRET")))
-		//			};
-		//		});
-		//}
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+				.AddJwtBearer(options =>
+				{
+					options.SaveToken = true;
+					options.RequireHttpsMetadata = false;
+					options.TokenValidationParameters = new TokenValidationParameters()
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidAudience = Env.GetString("JWT_VALID_AUDIENCE"),
+						ValidIssuer = Env.GetString("JWT_VALID_ISSUER"),
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Env.GetString("JWT_SECRET")))
+					};
+				});
+		}
 	}
 }
